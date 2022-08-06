562b0ed67b1798eb959b23e7ef503605d073a74e
br4e <br4e@protonmail.com>
2022-08-06 11:26:42 -0700

    beautify

--- src/Items/Pickups.c
+++ src/Items/Pickups.c
@@ -35,7 +35,7 @@ static void MoveNest(ObjNode *theNode);
 #define WhoHasPickUp	SpecialRef[0]	// objnode of object who has this pickup (nil == none)
 #define HoldingLimb		Special[1]		// which limb obj above object is doing holding
 #define	OldCType		Special[2]		// keeps old collision info when an obj is being held
-#define	OldCBits		Special[3]	
+#define	OldCBits		Special[3]
 
 #define	EggIsInPortal	Flag[0]			// when egg is inside time portal and being transported
 
@@ -57,8 +57,8 @@ ObjNode	*newObj;
 			/* MAKE THE EGG */
 			/****************/
 
-	gNewObjectDefinition.group = LEVEL0_MGroupNum_Egg;	
-	gNewObjectDefinition.type = LEVEL0_MObjType_Egg1 + itemPtr->parm[0];	
+	gNewObjectDefinition.group = LEVEL0_MGroupNum_Egg;
+	gNewObjectDefinition.type = LEVEL0_MObjType_Egg1 + itemPtr->parm[0];
 	gNewObjectDefinition.coord.x = x;
 	gNewObjectDefinition.coord.y = GetTerrainHeightAtCoord_Planar(x,z)-5.0f;
 	gNewObjectDefinition.coord.z = z;
@@ -75,24 +75,24 @@ ObjNode	*newObj;
 
 
 			/* SET COLLISION INFO */
-			
+
 	newObj->CType = CTYPE_MISC|CTYPE_PICKUP;
 	newObj->CBits = CBITS_TOUCHABLE;
-	
+
 	SetObjectCollisionBounds(newObj,10,-7,-15,15,15,-15);
-	
+
 	newObj->PickUpCollisionRadius = newObj->Radius * 4.0f;	// set pickup radius
 	newObj->WhoHasPickUp = nil;					// noone is holding this yet
 	newObj->EggIsInPortal = false;				// egg isn't in portal
 	newObj->Kind = itemPtr->parm[0];			// remember species of egg
-	
+
 
 
 			/* MAKE NEST */
-			
+
 	if (itemPtr->parm[3] & 1)
 		MakeNest(x,z);
-			
+
 
 	return(true);								// item was added
 }
@@ -108,12 +108,12 @@ short	species;
 
 
 		/* SEE IF IT'S INSIDE THE ACTIVE TIME PORTAL */
-		
-	
+
+
 	if (DoSimplePointCollision(&gCoord,CTYPE_PORTAL))
-	{		
+	{
 		ObjNode	*whoHasMe;
-		
+
 			/* EGG HAS BEEN RECOVERED */
 
 		whoHasMe = (ObjNode *)theNode->WhoHasPickUp;			// who has this?
@@ -127,22 +127,22 @@ short	species;
 		}
 		else
 			AddToScore(EGG_POINTS2);				// get smaller points
-		
+
 
 		theNode->TerrainItemPtr = nil;				// aint never comin back
-		
+
 		theNode->EggIsInPortal = true;				// it's being transported
 		theNode->MoveCall = MoveEggInPortal;		// change move routine
 		theNode->Health = 1.0;
 		theNode->Delta.y = 0;
 		theNode->CType = 0;
-		
+
 		if (SeeIfAllEggSpeciesRecovered())			// get extra points for getting all eggs
 			AddToScore(EGG_POINTS3);				// get winning points
-		
+
 		PlayEffect(EFFECT_PORTAL);
-		
-		UpdateObjectTransforms(theNode);		
+
+		UpdateObjectTransforms(theNode);
 	}
 }
 
@@ -158,24 +158,24 @@ float	fps = gFramesPerSecondFrac;
 
 	theNode->Delta.y += 220.0f * fps;						// climb up
 	theNode->Coord.y += theNode->Delta.y * fps;
-	
+
 	theNode->Health -= fps * 0.4f;						// decay it
 	if (theNode->Health <= 0.0f)
 	{
 		DeleteObject(theNode);
 		return;
-	}		
+	}
 
 			/* KEEP SPINNING */
-			
-	theNode->Rot.x += theNode->RotDelta.x*fps;				
-	theNode->Rot.y += theNode->RotDelta.y*fps;				
-	theNode->Rot.z += theNode->RotDelta.z*fps;				
+
+	theNode->Rot.x += theNode->RotDelta.x*fps;
+	theNode->Rot.y += theNode->RotDelta.y*fps;
+	theNode->Rot.z += theNode->RotDelta.z*fps;
 
 
 			/* UPDATE IT */
-			
-	MakeObjectTransparent(theNode,theNode->Health);	
+
+	MakeObjectTransparent(theNode,theNode->Health);
 	UpdateObjectTransforms(theNode);
 
 }
@@ -187,8 +187,8 @@ static void MakeNest(long  x, long z)
 {
 ObjNode	*newObj;
 
-	gNewObjectDefinition.group = LEVEL0_MGroupNum_Nest;	
-	gNewObjectDefinition.type = LEVEL0_MObjType_Nest;	
+	gNewObjectDefinition.group = LEVEL0_MGroupNum_Nest;
+	gNewObjectDefinition.type = LEVEL0_MObjType_Nest;
 	gNewObjectDefinition.coord.x = x;
 	gNewObjectDefinition.coord.y = GetTerrainHeightAtCoord_Planar(x,z);
 	gNewObjectDefinition.coord.z = z;
@@ -203,10 +203,10 @@ ObjNode	*newObj;
 
 
 			/* SET COLLISION INFO */
-			
+
 	newObj->CType = CTYPE_MISC;
 	newObj->CBits = CBITS_ALLSOLID;
-	
+
 	CreateCollisionTrianglesForObject(newObj);		// build triangle list
 }
 
@@ -220,7 +220,7 @@ static void MoveNest(ObjNode *theNode)
 		DeleteObject(theNode);
 		return;
 	}
-		
+
 	theNode->Coord.y = GetTerrainHeightAtCoord_Planar(theNode->Coord.x, theNode->Coord.z);
 	UpdateObjectTransforms(theNode);
 }
@@ -241,28 +241,28 @@ float	iScale;
 			/***********************/
 			/* SEE IF IS PICKED UP */
 			/***********************/
-			
+
 	if (theNode->WhoHasPickUp)
 	{
 		holderObj = (ObjNode *)theNode->WhoHasPickUp;						// get owner of pickup
 		limbNum = theNode->HoldingLimb;										// which limb of owner is holding this?
-		
-		
+
+
 		if (holderObj->CType == INVALID_NODE_FLAG)							// make sure holder is legal
 			return;
-		
-		
+
+
 			/* BUILD TRANSFORM MATRIX FOR PICKED UP OBJECT */
 			//
 			// Since the limb's matrix may contain scaling info, we need
 			// to cancel that out since we don't want our picked up
 			// object to be scaled.
 			//
-				
+
 		iScale = (1.0f/holderObj->Scale.x);									// calc amount to reverse scaling by
-		
+
 					/* SET SCALE-TRANS MATRIX MANUALLY (FASTER) */
-					
+
 		matrix.value[0][0] = iScale*theNode->Scale.x;	// scale x
 		matrix.value[1][1] = iScale*theNode->Scale.y;	// scale y
 		matrix.value[2][2] = iScale*theNode->Scale.z;	// scale z
@@ -283,39 +283,39 @@ float	iScale;
 		FindJointFullMatrix(holderObj,limbNum,&matrix2);						// get full matrix for mouth
 		Q3Matrix4x4_Multiply(&matrix,&matrix2,&theNode->BaseTransformMatrix);	// concat final matrix
 	}
-	
+
 			/*****************/
 			/* NOT PICKED UP */
 			/*****************/
-			
+
 	else
 	{
 		float	y,f;
-		
+
 		if (TrackTerrainItem(theNode))								// check to see if it's gone
 		{
 			DeleteObject(theNode);
 			return;
 		}
-						
+
 		GetObjectInfo(theNode);
-		
+
 					/* DO GRAVITY & FRICTION */
-					
-					
+
+
 		gDelta.y += -(GRAVITY_CONSTANT/2)*gFramesPerSecondFrac;		// add gravity
 
 		f = 200.0f * gFramesPerSecondFrac;								// calc friction value
-		
+
 		ApplyFrictionToDeltas(f,&gDelta);
-		
+
 
 					/* MOVE IT */
-					
+
 		gCoord.y += gDelta.y*gFramesPerSecondFrac;					// move it
 		gCoord.x += gDelta.x*gFramesPerSecondFrac;
 		gCoord.z += gDelta.z*gFramesPerSecondFrac;
-		
+
 		y = GetTerrainHeightAtCoord_Planar(gCoord.x, gCoord.z);		// get y here
 		if ((gCoord.y+theNode->BottomOff) < y)						// see if bottom below/on ground
 		{
@@ -325,27 +325,27 @@ float	iScale;
 				gDelta.y = 0;
 			gDelta.x *= .9f;										// strong friction on landing
 			gDelta.z *= .9f;
-			
+
 			theNode->RotDelta.x *= .8f;
 			theNode->RotDelta.y *= .8f;
 			theNode->RotDelta.z *= .8f;
 		}
-		
+
 			/* DEAL WITH SLOPES */
-			
+
 		if (gRecentTerrainNormal.y < .95f)							// if fairly flat, then no sliding effect
-		{	
+		{
 			gDelta.x += gRecentTerrainNormal.x * gFramesPerSecondFrac * 900.0f;
 			gDelta.z += gRecentTerrainNormal.z * gFramesPerSecondFrac * 900.0f;
-		}		
-		
-		
+		}
+
+
 				/* SPIN IT */
 
-		theNode->Rot.x += theNode->RotDelta.x*gFramesPerSecondFrac;				
-		theNode->Rot.y += theNode->RotDelta.y*gFramesPerSecondFrac;				
-		theNode->Rot.z += theNode->RotDelta.z*gFramesPerSecondFrac;				
-				
+		theNode->Rot.x += theNode->RotDelta.x*gFramesPerSecondFrac;
+		theNode->Rot.y += theNode->RotDelta.y*gFramesPerSecondFrac;
+		theNode->Rot.z += theNode->RotDelta.z*gFramesPerSecondFrac;
+
 		UpdateObject(theNode);
 	}
 }
@@ -368,14 +368,14 @@ ObjNode				*pickedObj;
 
 
 			/* FIND COORDINATE OF MOUTH */
-										
+
 	FindCoordOnJoint(theNode, limbNum, &inPoint, &testPt);
-	
-	
+
+
 			/* DO COLLISION DETECT TO FIND OBJECT */
-					
+
 	pickedObj = IsPointInPickupCollisionSphere(&testPt);	// see if this coord is inside a pickup's collision sphere
-					
+
 	if (pickedObj)
 	{
 		theNode->StatusBits |= STATUS_BIT_ISCARRYING;
@@ -383,11 +383,11 @@ ObjNode				*pickedObj;
 		pickedObj->WhoHasPickUp = theNode;
 		pickedObj->HoldingLimb = limbNum;
 		pickedObj->OldCBits = pickedObj->CBits;				// keep collision settings
-		pickedObj->OldCType = pickedObj->CType;	
+		pickedObj->OldCType = pickedObj->CType;
 		pickedObj->CBits = 0;								// clear collision stuff while being carried
 		pickedObj->CType = 0;
 		pickedObj->StatusBits |= STATUS_BIT_DONTCULL;		// dont do custom culling while being carried
-		
+
 	}
 }
 
@@ -418,9 +418,9 @@ static const TQ3Point3D	inPoint = {0,0,0};
 	itemObj->Delta.y = 0;											// start falling momentum @ 0
 
 	itemObj->Rot.y = theNode->Rot.y;								// match y rotations
-	
+
 			/* GET CURRENT COORD OF ITEM */
-			
+
 	Q3Point3D_Transform(&inPoint, &itemObj->BaseTransformMatrix, &itemObj->Coord);
 }
 
@@ -430,9 +430,9 @@ static const TQ3Point3D	inPoint = {0,0,0};
 static Boolean SeeIfAllEggSpeciesRecovered(void)
 {
 int	i,n;
-	
+
 			/* COUNT # SPECIES GOTTEN */
-			
+
 	n = 0;
 	for (i = 0; i < NUM_EGG_SPECIES; i++)
 		if (gRecoveredEggs[i] > 0)
@@ -440,15 +440,15 @@ int	i,n;
 
 
 			/* SEE IF ALL ACCOUNTED FOR */
-				
+
 	if (n >= NUM_EGG_SPECIES)
 	{
 		MakeTimePortal(PORTAL_TYPE_EXIT, gMyCoord.x, gMyCoord.z);
 		MorphToSkeletonAnim(gPlayerObj->Skeleton, PLAYER_ANIM_EXIT, 3);
 		gPlayerObj->Flag[0] = false;
 		gPlayerObj->ExitTimer = 0;
-		
-		PlayEffect_Parms(EFFECT_PORTAL,FULL_CHANNEL_VOLUME,kMiddleC-8);		
+
+		PlayEffect_Parms(EFFECT_PORTAL,FULL_CHANNEL_VOLUME,kMiddleC-8);
 		return(true);
 	}
 	return(false);
@@ -468,10 +468,3 @@ int	i;
 
 	SeeIfAllEggSpeciesRecovered();
 }
-
-
-
-
-
-
-
